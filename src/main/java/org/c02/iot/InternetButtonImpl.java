d9bb52c6647774e63cc0adccf958b726e9d5776b
Philipp <Philipp@172.31.36.75>
2017-02-04 10:49:10 +0100

    demo led

--- src/main/java/org/c02/iot/InternetButtonImpl.java
+++ src/main/java/org/c02/iot/InternetButtonImpl.java
@@ -3,6 +3,7 @@ package org.c02.iot;
 import java.awt.Color;
 
 import org.c02.iot.cloud.api.ParticleApiWrapper;
+import org.c02.iot.cloud.api.ParticleException;
 
 public class InternetButtonImpl implements InternetButtonApi {
 
@@ -17,13 +18,25 @@ public class InternetButtonImpl implements InternetButtonApi {
 		return 0;
 	}
 
-	public void setLed(int postition, Color color) {
+	public void setLed(int position, String color/*Color color*/)  {
 		// TODO Auto-generated method stub
+		try {
+			wrapper.callMethod("led",position+color.toString());
+		} catch (ParticleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 	public void allLedsOff() {
 		// TODO Auto-generated method stub
-
+        try {
+			wrapper.callMethod("ledsOff",null);
+		} catch (ParticleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void playSound() {
@@ -32,6 +45,12 @@ public class InternetButtonImpl implements InternetButtonApi {
 	}
 
 	public void resetButtonCounters() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLed(int postition, Color color) {
 		// TODO Auto-generated method stub
 		
 	}
