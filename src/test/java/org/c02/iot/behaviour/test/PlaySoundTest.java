5947c161d86242530c719b0945d9b918faff0bee
david <david@DESKTOP-17B0LL5>
2017-02-04 10:57:29 +0100

    Build Error gefixed

--- src/test/java/org/c02/iot/behaviour/test/PlaySoundTest.java
+++ src/test/java/org/c02/iot/behaviour/test/PlaySoundTest.java
@@ -3,16 +3,18 @@ package org.c02.iot.behaviour.test;
 import java.awt.Color;
 
 import org.c02.iot.InternetButtonApi;
+import org.c02.iot.cloud.api.ParticleApiWrapper;
+import org.c02.iot.cloud.api.ParticleApiWrapperImpl;
+import org.c02.iot.cloud.examples.DemoConstants;
 
 public class PlaySoundTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 	
 	public void testPlaySound()
 	{
+		ParticleApiWrapper api = new ParticleApiWrapperImpl(DemoConstants.deviceId,
+				DemoConstants.accessToken);
+		
 		//InternetButtonApi buttonInstance = new InternetButtonApi()
 
 	}
