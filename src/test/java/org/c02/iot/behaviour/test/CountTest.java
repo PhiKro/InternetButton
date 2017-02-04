b1ba49da97f105108d044147135e9867be63d84c
david <david@DESKTOP-17B0LL5>
2017-02-04 13:16:36 +0100

    fix Count Test

--- src/test/java/org/c02/iot/behaviour/test/CountTest.java
+++ src/test/java/org/c02/iot/behaviour/test/CountTest.java
@@ -5,14 +5,10 @@ package org.c02.iot.behaviour.test;
 import org.c02.iot.InternetButtonApi.ButtonDirection;
 import org.c02.iot.InternetButtonImpl;
 import org.c02.iot.cloud.api.ParticleApiWrapper;
-import org.c02.iot.cloud.api.ParticleApiWrapperImpl;
-import org.c02.iot.cloud.examples.DemoConstants;
 import org.junit.Test;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-
 import java.io.IOException;
-import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class CountTest {
 
@@ -21,7 +17,7 @@ public class CountTest {
 		
 		ParticleApiWrapper api = mock(ParticleApiWrapper.class);
 		InternetButtonImpl test = new InternetButtonImpl(api);
-		
+	
 		test.getButtonCounter(ButtonDirection.North);
 		try {
 			verify(api).readVariable("countButton1");
@@ -29,14 +25,7 @@ public class CountTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
-		
-		 // api.getButtonCounter(bu);
-		
-		
+
 	}
 
 }
