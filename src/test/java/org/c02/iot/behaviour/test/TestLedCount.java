cc0256152857575e1ea1f96de8b17e419d986fa1
david <david@DESKTOP-17B0LL5>
2017-02-04 14:17:22 +0100

    Test f 2.1 und 2.2

--- src/test/java/org/c02/iot/behaviour/test/TestLedCount.java
+++ src/test/java/org/c02/iot/behaviour/test/TestLedCount.java
@@ -4,31 +4,27 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import org.c02.iot.InternetButtonImpl;
-import org.c02.iot.behaviour.CountandMoreRed;
+import org.c02.iot.behaviour.CountAndShowLed;
 import org.c02.iot.cloud.api.ParticleApiWrapper;
 import org.c02.iot.cloud.api.ParticleException;
 import org.junit.Test;
 
-public class TestLedColor {
+public class TestLedCount {
 
 	@Test
 	public void test() {
-		
 		ParticleApiWrapper api = mock(ParticleApiWrapper.class);
 		InternetButtonImpl CountDemo = new InternetButtonImpl (api);
-		CountandMoreRed newRed = new CountandMoreRed(CountDemo);
-		newRed.run(1);
+		CountAndShowLed newCount = new CountAndShowLed(CountDemo);
+		
+		newCount.run();
 		try {
 			verify(api).callMethod("ledsOff", null);
-			verify(api).callMethod("reset", null);
-			verify(api).callMethod("led", "051515030");
+			verify(api).callMethod("led", "0002550");
 		} catch (ParticleException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		//fail("Not yet implemented");
 	}
 
 }
