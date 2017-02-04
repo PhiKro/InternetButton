3f55247ea56fa17246797524cba5fdbf991c95aa
david <david@DESKTOP-17B0LL5>
2017-02-04 10:04:38 +0100

    Device ID hinzugefuegt

---
+++ src/main/java/org/c02/connection_util/connection.java
@@ -0,0 +1,10 @@
+package org.c02.connection_util;
+
+import org.c02.iot.cloud.api.ParticleApiWrapperImpl;
+
+public class connection {
+	ParticleApiWrapperImpl x = new ParticleApiWrapperImpl("3a0040001347343432313031", "gruppe2");
+	
+	
+
+}
